89ff813bfaba2e19be9fff69e710f904d30d30a3
DenisChuvilin <denischuv@gmail.com>
2020-07-24 10:56:05 -0700

    not done yet

--- api/server.js
+++ api/server.js
@@ -2,12 +2,36 @@ const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
 
+const session = require('express-session');
+const KnexSessionStore = require('connect-session-knex')(session);
+
 const authenticate = require('../auth/authenticate-middleware.js');
 const authRouter = require('../auth/auth-router.js');
 const jokesRouter = require('../jokes/jokes-router.js');
+const dbConfig = require('../database/dbConfig.js');
 
 const server = express();
 
+const sessionConfiguration = {
+  name: 'monster', // default value is sid
+  secret: process.env.SESSION_SECRET || 'keep it secret, keep it safe!', // key for encryption
+  cookie: {
+    maxAge: 1000 * 60 * 10,
+    secure: process.env.USE_SECURE_COOKIES || false, // send the cookie only over https (secure connections)
+    httpOnly: true, // prevent JS code on client from accessing this cookie
+  },
+  resave: false,
+  saveUninitialized: true, // read docs, it's related to GDPR compliance
+  store: new KnexSessionStore({
+    knex: dbConfig,
+    tablename: 'sessions',
+    sidfieldname: 'sid',
+    createtable: true,
+    clearInterval: 1000 * 60 * 30, // time to check and remove expired sessions from database
+  }),
+};
+
+server.use(session(sessionConfiguration)); // enables session support
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,47 @@
+const bcryptjs = require('bcryptjs');
 const router = require('express').Router();
+
+const Users = require('../users/users-model');
 
 router.post('/register', (req, res) => {
   // implement registration
+  let creds = req.body;
+  const rounds = process.env.HASH_ROUNDS || 4;
+
+  const hash = bcryptjs.hashSync(creds.password, rounds);
+
+  creds.password = hash;
+
+  Users.add(creds)
+    .then((saved) => {
+      console.log(saved);
+      res.status(201).json({ data: saved });
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json({ error: error.message });
+    });
 });
 
 router.post('/login', (req, res) => {
   // implement login
+  const { username, password } = req.body;
+
+  Users.findBy({ username })
+    .then((users) => {
+      const user = users[0];
+      if (user && bcryptjs.compareSync(password, user.password)) {
+        req.session.loggedIn = true;
+        req.session.username = user.username;
+
+        res.status(200).json({ message: 'welcome', session: req.session });
+      } else {
+        res.status(401).json({ message: 'Invalid Credentials' });
+      }
+    })
+    .catch((error) => {
+      res.status(500).json({ error: error.message });
+    });
 });
 
 module.exports = router;
